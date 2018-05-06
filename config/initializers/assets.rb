9896592ceb95c5c9be622e4477ca281c8600f396
Patryk Ptasinski <github@ipepe.pl>
2018-05-06 23:54:54 +0200

    Fixed assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,5 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.digest = true
-Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .woff2 .png)
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'font-awesome', 'fonts')
+Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.woff2 *.png)
 
-NonStupidDigestAssets.whitelist += [/.(?i:)(?:svg|eot|woff|ttf|otf)$/]
+NonStupidDigestAssets.whitelist += [/.(?i:)(?:svg|eot|woff|ttf|woff2|png)$/]
